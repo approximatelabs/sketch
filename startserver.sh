b126131b11c7fa90bb8ab88c750230867b7dbe80
Justin Waugh <916073+bluecoconut@users.noreply.github.com>
2022-07-20 17:30:13 -0500

    add tailwind properly and update running

--- startserver.sh
+++ startserver.sh
@@ -1,1 +1,10 @@
-DEBUG=True uvicorn sketch.api.main:app --reload
+#!/bin/bash
+
+trap 'kill $(jobs -p)' SIGINT
+
+kill $(lsof -t -i:8000)
+# you should be in valid node version and stuff
+(cd ./sketch/api/tailwindcss && npx tailwindcss -i ./src/input.css -o ../static/output.css --watch &)
+# You should be in a valid python environment
+DEBUG=True uvicorn sketch.api.main:app --reload &
+
