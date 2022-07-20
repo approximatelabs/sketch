c0b3f8889387223dab353287c7b723aeab018351
Justin Waugh <916073+bluecoconut@users.noreply.github.com>
2022-07-20 18:49:30 -0500

    some updates, header and organization

--- startserver.sh
+++ startserver.sh
@@ -2,9 +2,14 @@
 
 trap 'kill $(jobs -p)' SIGINT
 
-kill $(lsof -t -i:8000)
 # you should be in valid node version and stuff
 (cd ./sketch/api/tailwindcss && npx tailwindcss -i ./src/input.css -o ../static/output.css --watch &)
+PID1=$!
+
+echo $PID1
 # You should be in a valid python environment
 DEBUG=True uvicorn sketch.api.main:app --reload &
+PID2=$!
 
+wait $PID1
+wait $PID2
