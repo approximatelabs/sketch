1221acaa51c03d85bf4e0a23291901651ee1150d
Justin <916073+bluecoconut@users.noreply.github.com>
2022-07-23 20:12:08 +0000

    maybe thats better

--- startserver.sh
+++ startserver.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-trap 'kill $(jobs -p | xargs)' SIGINT SIGHUP SIGTERM EXIT
-
 # you should be in valid node version and stuff
-(cd ./sketch/api/tailwindcss && npx tailwindcss -i ./src/input.css -o ../static/output.css --watch &)
+(cd ./sketch/api/tailwindcss && npx tailwindcss -i ./src/input.css -o ../static/output.css --watch) &
 PID1=$!
 
-echo $PID1
 # You should be in a valid python environment
-(cd ./sketch && DEBUG=True uvicorn sketch.api.main:app --reload &)
+(cd ./sketch && DEBUG=True uvicorn sketch.api.main:app --reload) &
 PID2=$!
 
 wait $PID1
