cf669d6ee511fb5e619936008776792553020d43
Justin Waugh <916073+bluecoconut@users.noreply.github.com>
2022-09-20 11:31:33 -0600

    basic chat bot

--- clean_juypter.sh
+++ clean_juypter.sh
@@ -4,4 +4,7 @@
 # find . -name "*.ipynb" -exec jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {} \;
 
 # Bash command to convert all jupyter notebooks in git diff to have no output
-git diff --name-only --diff-filter=ACMRTUXB | grep ".ipynb" | xargs -I {} jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}
+git diff --cached --name-only --diff-filter=ACMRTUXB | grep ".ipynb" | xargs -I {} jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}
+
+# # find any instance of a notebook with the text "dotenv" in it
+# find . -name "*.ipynb" -exec grep -H "dotenv" {} \;
