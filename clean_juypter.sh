2b832bab6c1b902e2aed70885660de36a739442a
Justin Waugh <916073+bluecoconut@users.noreply.github.com>
2022-09-20 13:28:29 -0600

    some basic query stuff

--- clean_juypter.sh
+++ clean_juypter.sh
@@ -5,6 +5,7 @@
 
 # Bash command to convert all jupyter notebooks in git diff to have no output
 git diff --cached --name-only --diff-filter=ACMRTUXB | grep ".ipynb" | xargs -I {} jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}
+git diff --name-only --diff-filter=ACMRTUXB | grep ".ipynb" | xargs -I {} jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {}
 
 # # find any instance of a notebook with the text "dotenv" in it
 # find . -name "*.ipynb" -exec grep -H "dotenv" {} \;
